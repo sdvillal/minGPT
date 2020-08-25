d88474c21b2b19dab2ddc7815764af4ac0027089
Santi Villalba <sdvillal@gmail.com>
2020-08-25 20:44:21 +0200

    add semi-auto-generated setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='mingpt',
+    version='0.1.1jc',
+    packages=['mingpt', 'mingpt.demos'],
+    url='',
+    license='',
+    author='karpathy',
+    author_email='',
+    description='',
+    install_requires=['tqdm', 'numpy', 'matplotlib',
+                      'pytorch', 'torchvision']
+)
